43b3d2ae274f46130746a14edac11bb14284ce48
Félix Bouveret <bouveret.felix@gmail.com>
2020-09-24 17:54:39 +0200

    Add verifyPosition

--- index.php
+++ index.php
@@ -8,13 +8,27 @@ function initGrid()
   for ($i = 0; $i < 10; $i++) {
     array_push($grid, []);
     for ($o = 0; $o < 10; $o++) {
-      array_push($grid[$i], []);
+      array_push($grid[$i], [
+        'targeted' => false,
+        'isBoat' => false
+      ]);
     }
   };
   return $grid;
 };
 
-function displayGrid()
+
+function verifyPosition($x, $y, &$the_grid)
+{
+  if ($the_grid[$x - 1][$y - 1]['targeted'] === true) {
+    return 'X ';
+  } else {
+    return '• ';
+  };
+};
+
+
+function displayGrid(&$the_grid)
 {
   $displayGrid = [];
 
@@ -28,36 +42,31 @@ function displayGrid()
       if ($i === 0 || $i === 11) {
         array_push($column, "* ");
         print_r('* ');
-        
       } else {
-        
+
         if ($o === 0 || $o === 11) {
-          
+
           print_r('* ');
           array_push($column, "* ");
-          
         } else {
-          
-          print_r('• ');
-          array_push($column, "• ");
+          $verification = verifyPosition($o, $i, $the_grid);
 
+          print_r($verification);
+          array_push($column, $verification);
         }
-
       };
 
       if ($o === 11) {
 
         print_r("\n\r");
-
       }
-
     };
     array_push($displayGrid, $column);
   };
 
   return $displayGrid;
-
 };
+
 
 
 function turn($handl, &$the_grid)
@@ -65,13 +74,14 @@ function turn($handl, &$the_grid)
   echo "Coordonnées (x,y): ";
   $coord = fgets($handl);
   $array_coord = array_map('intval', explode(',', $coord));
-  
-  $the_grid[$array_coord[0]][$array_coord[1]] = 'caca';
+
+  $the_grid[$array_coord[0] - 1][$array_coord[1] - 1]["targeted"] = true;
+  displayGrid($the_grid);
+  turn($handl, $the_grid);
 };
 
-// $grid = initGrid();
-$ret = displayGrid();
+$grid = initGrid();
+displayGrid($grid);
 $test = 0;
 
 turn($handler, $grid);
-// var_dump($grid);
