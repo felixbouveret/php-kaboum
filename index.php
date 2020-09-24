4f75f143bbc38947bc13f72ed48e75758da24aa0
Benoit Lamonica <ben33127@gmail.com>
2020-09-24 17:32:58 +0200

    Added Array display

--- index.php
+++ index.php
@@ -16,24 +16,48 @@ function initGrid()
 
 function displayGrid()
 {
-  for ($i = 0; $i < 12; $i++) {
-    for ($o = 0; $o < 12; $o++) {
-      if ($i === 0 || $i === 11) {
-        print_r('* ');
-      } else {
-        if ($o === 0 || $o === 11) {
-          print_r('* ');
-        } else {
-          print_r('• ');
-        }
-      };
-      if ($o === 11) {
-        print_r("\n\r");
-      }
-    };
-  };
-};
+  $displayGrid = [];
 
+  for ($i = 0; $i < 12; $i++) {
+
+    $column = [];
+
+    for ($o = 0; $o < 12; $o++) {
+
+
+      if ($i === 0 || $i === 11) {
+        array_push($column, "* ");
+        print_r('* ');
+        
+      } else {
+        
+        if ($o === 0 || $o === 11) {
+          
+          print_r('* ');
+          array_push($column, "* ");
+          
+        } else {
+          
+          print_r('• ');
+          array_push($column, "• ");
+
+        }
+
+      };
+
+      if ($o === 11) {
+
+        print_r("\n\r");
+
+      }
+
+    };
+    array_push($displayGrid, $column);
+  };
+
+  return $displayGrid;
+
+};
 
 
 function turn($handl, &$the_grid)
@@ -41,11 +65,13 @@ function turn($handl, &$the_grid)
   echo "Coordonnées (x,y): ";
   $coord = fgets($handl);
   $array_coord = array_map('intval', explode(',', $coord));
-
+  
   $the_grid[$array_coord[0]][$array_coord[1]] = 'caca';
 };
 
 // $grid = initGrid();
-displayGrid();
-// turn($handler, $grid);
+$ret = displayGrid();
+$test = 0;
+
+turn($handler, $grid);
 // var_dump($grid);
