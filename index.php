063a96228d4062a90f3d97f7b5a09a50233cdcfe
Félix Bouveret <bouveret.felix@gmail.com>
2020-09-24 17:18:02 +0200

    Add displayGrid

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $handler = fopen("php://stdin", "r");
 
+
 function initGrid()
 {
   $grid = array();
@@ -13,18 +14,38 @@ function initGrid()
   return $grid;
 };
 
+function displayGrid()
+{
+  for ($i = 0; $i < 12; $i++) {
+    for ($o = 0; $o < 12; $o++) {
+      if ($i === 0 || $i === 11) {
+        print_r('* ');
+      } else {
+        if ($o === 0 || $o === 11) {
+          print_r('* ');
+        } else {
+          print_r('• ');
+        }
+      };
+      if ($o === 11) {
+        print_r("\n\r");
+      }
+    };
+  };
+};
 
 
-function turn($handl)
+
+function turn($handl, &$the_grid)
 {
   echo "Coordonnées (x,y): ";
   $coord = fgets($handl);
   $array_coord = array_map('intval', explode(',', $coord));
 
-  var_dump($array_coord);
+  $the_grid[$array_coord[0]][$array_coord[1]] = 'caca';
 };
 
-$grid = initGrid();
-var_dump($grid);
-
-turn($handler);
+// $grid = initGrid();
+displayGrid();
+// turn($handler, $grid);
+// var_dump($grid);
